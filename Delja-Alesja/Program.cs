d0e16b8b4fbff417d7b4d43b433b38d47363b0af
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2022-05-03 21:48:20 +0200

    Init solution

---
+++ Delja-Alesja/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Delja_Alesja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
