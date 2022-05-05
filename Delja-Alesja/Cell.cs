769e39519348243fa6d50f46467c7d4916c76c9f
Alesja Delja <alesja.delja@studio.unibo.it>
2022-05-05 16:28:11 +0200

    Try of the getter

--- Delja-Alesja/Cell.cs
+++ Delja-Alesja/Cell.cs
@@ -4,6 +4,7 @@ namespace Delja_Alesja
 {
     class Cell
     {
+        private const int V = 0;
         private readonly int type;
         private readonly int position;
         private bool discovered;
@@ -18,6 +19,13 @@ namespace Delja_Alesja
         /*
          * Manca solo l'handler, il listener e i metodi usati dall'handler per i due modi di premere il bottone
          */
+
+        /**
+     *@return method that returns the type.
+     * - 0 if the cell is empty
+     * - 1 if there is a mine
+     * - 2 of there is a number
+     */
         public int Type => type;
         public int Position => position;
 
@@ -28,7 +36,27 @@ namespace Delja_Alesja
         public void SetFlagged(bool flag) => this.flagged = flag;
         static void Main(string[] args)
         {
-            Console.WriteLine("prova Cell!");
+            Cell cell = new Cell(3,1,false,false);
+            Console.WriteLine("Prova di Cell!");
+            Console.WriteLine("Tipo cella: " + cell.Type);
+            if (cell.type == 0)
+            {
+                Console.WriteLine("Cella Vuota");
+            }
+            else if (cell.type == 1)
+            {
+                Console.WriteLine("Cella Con Mina");
+            }
+            else if (cell.type == 2)
+            {
+                Console.WriteLine("Cella Con Numero");
+            }else
+            { 
+                Console.WriteLine("Hai inserito un numero non esatto"); 
+            }
+            Console.WriteLine("Posizione cella: " + cell.Position);
+            Console.WriteLine("Cella scoperta: " + cell.IsDiscovered);
+            Console.WriteLine("Cella con flag: " + cell.Flagged);
         }
     }
 }
