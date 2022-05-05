8611fc34b074fdd4300d9f35a084e08ccacb0591
Silvia Furegato <silvia.furegato@studio.unibo.it>
2022-05-05 19:42:11 +0200

    Create empty classes and interfaces

---
+++ Furegato-Silvia/Cell.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Furegato_Silvia
+{
+    class Cell
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
