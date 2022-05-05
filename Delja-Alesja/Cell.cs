0842a6f9b3de3faaf908b4d2df2d6769447767ac
Alesja Delja <alesja.delja@studio.unibo.it>
2022-05-05 15:58:54 +0200

    Created the class Cell with the getter/setter

---
+++ Delja-Alesja/Cell.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Delja_Alesja
+{
+    class Cell
+    {
+        private readonly int type;
+        private readonly int position;
+        private bool discovered;
+        private readonly bool flagged;
+
+        public bool Flagged => flagged;
+
+        public Cell(int type, int position, bool discovered, bool flagged) {
+            this.type = type;
+            this.position = position;
+            this.discovered = discovered;
+            this.flagged = flagged;
+        }
+
+        public new int GetType()
+        {
+            return type;
+        }
+        public int GetPosition()
+        {
+            return position;
+        }
+
+        public bool IsDiscovered()
+        {
+            return discovered;
+        }
+
+        public void SetDiscovered(bool discover)
+        {
+            this.discovered = discover;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("prova Cell!");
+        }
+    }
+}
